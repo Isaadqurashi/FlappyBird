0ca112d946cb067f251b7571555fe074b07bdca4
Saad Khurshid Qurashi <saadkhurshidqureshi@gmail.com>
2024-06-07 22:06:51 +0500

    added data in AssetManager.hpp

--- AssetManager.hpp
+++ AssetManager.hpp
@@ -1,1 +1,19 @@
 #pragma once
+
+#include <map>
+
+#include<SFML/Graphics.hpp>
+
+namespace Flappy
+{
+	class AssetManager
+	{
+	public:
+		AssetManager() { }
+		~AssetManager() { }
+
+		void LoadTexture(std::string name, std::string fileName);
+
+	};
+
+}
